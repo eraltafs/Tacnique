e00acae9eb4bcde7dbe3b05a9538798e8aa40892
eraltafs <43698833+eraltafs@users.noreply.github.com>
2023-10-08 12:35:20 +0530

    comments for future

--- controller/task.controller.js
+++ controller/task.controller.js
@@ -1,6 +1,6 @@
 const { taskModel } = require("../models/task.model");
-const { userModel } = require("../models/user.model");
 
+// Create a new task
 const task_add = async (req, res) => {
   const { user_id } = req.user;
   const { title, description } = req.body;
@@ -12,76 +12,91 @@ const task_add = async (req, res) => {
   try {
     const task = new taskModel({ title, description, creation_date, user_id });
     await task.save();
-    res.send({ msg: "task added", task });
+    res.status(201).send({ msg: "Task added successfully", task });
   } catch (error) {
-    console.log(error);
-    return res.send({ msg: "server error" });
+    console.error(error);
+    return res.status(500).send({ msg: "Server error" });
   }
 };
+
+// Get all tasks for a user
 const get_all_task = async (req, res) => {
   const { user_id } = req.user;
   try {
-    const task = await taskModel.find({ user_id });
-    res.send(task);
+    const tasks = await taskModel.find({ user_id });
+    res.status(200).send(tasks);
   } catch (error) {
-    console.log(error);
-    return res.send({ msg: "server error" });
+    console.error(error);
+    return res.status(500).send({ msg: "Server error" });
   }
 };
+
+// Get a task by its ID
 const get_task = async (req, res) => {
   const { id } = req.params;
   const { user_id } = req.user;
   try {
     const task = await taskModel.findOne({ _id: id });
     if (!task) {
-      return res.send({ msg: "task not found" });
+      return res.status(404).send({ msg: "Task not found" });
     }
     if (task.user_id == user_id) {
-      return res.send(task);
+      return res.status(200).send(task);
     }
-    return res.send({ msg: "you are not allowed to get another person task" });
+    return res
+      .status(403)
+      .send({ msg: "You are not allowed to get another person's task" });
   } catch (error) {
-    console.log(error);
-    return res.send({ msg: "server error" });
+    console.error(error);
+    return res.status(500).send({ msg: "Server error" });
   }
 };
+
+// Update a task by its ID
 const update_task = async (req, res) => {
   const { id } = req.params;
   const { user_id } = req.user;
-  const payload = req.body
+  const payload = req.body;
   try {
     const task = await taskModel.findOne({ _id: id });
     if (!task) {
-      return res.send({ msg: "task not found" });
+      return res.status(404).send({ msg: "Task not found" });
     }
     if (task.user_id == user_id) {
-      await taskModel.findByIdAndUpdate(id,payload)
-      return res.send({msg:"task updated"});
+      await taskModel.findByIdAndUpdate(id, payload);
+      return res.status(200).send({ msg: "Task updated successfully" });
     }
-    return res.send({ msg: "you are not allowed to update another person task" });
+    return res
+      .status(403)
+      .send({ msg: "You are not allowed to update another person's task" });
   } catch (error) {
-    console.log(error);
-    return res.send({ msg: "server error" });
+    console.error(error);
+    return res.status(500).send({ msg: "Server error" });
   }
 };
+
+// Delete a task by its ID
 const delete_task = async (req, res) => {
   const { id } = req.params;
   const { user_id } = req.user;
   try {
     const task = await taskModel.findOne({ _id: id });
     if (!task) {
-      return res.send({ msg: "task not found" });
+      return res.status(404).send({ msg: "Task not found" });
     }
     if (task.user_id == user_id) {
-      await taskModel.findByIdAndDelete(id)
-      return res.send({msg:"task deleted"});
+      await taskModel.findByIdAndDelete(id);
+      return res.status(200).send({ msg: "Task deleted successfully" });
     }
-    return res.send({ msg: "you are not allowed to delete another person task" });
+    return res
+      .status(403)
+      .send({ msg: "You are not allowed to delete another person's task" });
   } catch (error) {
-    console.log(error);
-    return res.send({ msg: "server error" });
+    console.error(error);
+    return res.status(500).send({ msg: "Server error" });
   }
 };
+
 module.exports = {
   task_add,
   get_all_task,
