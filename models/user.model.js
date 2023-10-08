7caaf46b669cb80068ad53e9043153059f540302
eraltafs <43698833+eraltafs@users.noreply.github.com>
2023-10-08 09:33:51 +0530

    created user model

---
+++ models/user.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  email: { type: String, require: true },
+  password: { type: String, require: true },
+  name: String,
+});
+
+const UserModel = mongoose.model("user", userSchema);
+
+module.exports = { UserModel };
